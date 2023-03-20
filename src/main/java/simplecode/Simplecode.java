a66b7f17f0a87309eedf333f667b6b11a31d9f40
Gayathri Kumaresan <gayathrikumaresan422@gmail.com>
2023-03-20 20:22:21 +0530

    Committing maven project

---
+++ src/main/java/simplecode/Simplecode.java
@@ -0,0 +1,11 @@
+package simplecode;
+
+public class Simplecode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Welcome to Simplecode");
+
+	}
+
+}
