2dc8a73dff468f5e31938c2812608f1e4b725cb2
Gayathri Kumaresan <gayathrikumaresan422@gmail.com>
2023-03-21 19:31:42 +0530

    Committing Maven Red

---
+++ eclipse-workspace/Red/src/main/java/Maven/Red/App.java
@@ -0,0 +1,13 @@
+package Maven.Red;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Maven Red Code.." );
+    }
+}
